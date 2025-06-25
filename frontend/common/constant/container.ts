51c3d55238c49ea54efa3d56e593269cfc80f2bc
tianxiang.fighting <tianxiang.fighting@bytedance.com>
2025-06-25 16:37:55 +0800

    feat: 更新版本号

--- frontend/common/constant/container.ts
+++ frontend/common/constant/container.ts
@@ -13,5 +13,8 @@ export enum ContainerTypeEnum {
 /**
  * 变更记录：
  * 1. 2025年6月9日 版本 v1.0.0
+ * 2. 2025年6月12日 版本 v1.0.1
+ * 3. 2025年6月18日 版本 v1.0.2
+ * 4. 2025年6月25日 版本 v1.0.3
  */
-export const BatchToolsVersion = 'v1.0.2';
+export const BatchToolsVersion = 'v1.0.3';
