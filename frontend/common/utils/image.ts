5bced7b6b608bec207f470fb145d2555e9d1aeb7
tianxiang.fighting <tianxiang.fighting@bytedance.com>
2025-06-27 17:57:23 +0800

    fix: 修复从输出中读取文件的批量参数配置问题

--- frontend/common/utils/image.ts
+++ frontend/common/utils/image.ts
@@ -1,7 +1,9 @@
 // Copyright (c) 2025 Bytedance Ltd. and/or its affiliates
 // SPDX-License-Identifier: GPL-3.0-or-later
 import { apiPrefix } from '@api/request-instance';
+import { SpecialOutputSuffix } from '@common/constant/params-config';
 import { isString } from 'lodash';
+import { getSpecialOutputValue } from './value-type';
 
 const RE_IMAGE_SUFFIX =
   /\.(bmp|jpg|png|tif|gif|pcx|tga|exif|fpx|svg|psd|cdr|pcd|dxf|ufo|eps|ai|raw|WMF|webp|jpeg)$/;
@@ -35,6 +37,12 @@ export const getImageUrlV2 = (
     return '';
   }
   const cache = useCache ? '' : `&cache=${Date.now()}}`;
+
+  if (fileName.endsWith(SpecialOutputSuffix)) {
+    type = 'output';
+    fileName = getSpecialOutputValue(fileName);
+    return `${apiPrefix}/view-image?file_name=${encodeURIComponent(fileName)}&type=${type}${cache}`;
+  }
 
   return `${apiPrefix}/view-image?file_name=${encodeURIComponent(fileName)}&type=${type}${cache}`;
 };
